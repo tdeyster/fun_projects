251bb88ff4ef597f0e37093c1d92597e97beb03a
Teddy Eyster <tdeyster@users.noreply.github.com>
2021-08-28 00:12:14 +1200

    google drive kml

--- run_where_you_want/index.js
+++ run_where_you_want/index.js
@@ -5,7 +5,8 @@ function initMap() {
     center: { lat: 41.876, lng: -87.624 },
   });
   const ctaLayer = new google.maps.KmlLayer({
-    url: "https://raw.githubusercontent.com/tdeyster/fun_projects/gh-pages/run_where_you_want/running.kml",
+    //url: "https://raw.githubusercontent.com/tdeyster/fun_projects/gh-pages/run_where_you_want/running.kml",
+    url: "https://drive.google.com/file/d/15QhrUe99R1gwgBW-t6a81lBS2qk-p9VA/view?usp=sharing",
     map: map,
   });
 }
