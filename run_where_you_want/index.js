d4634d0b0506df643c29a02441b977a2b90eb08c
Teddy Eyster <tdeyster@users.noreply.github.com>
2021-08-28 09:14:08 +1200

    Update index.js

--- run_where_you_want/index.js
+++ run_where_you_want/index.js
@@ -7,7 +7,8 @@ function initMap() {
   const ctaLayer = new google.maps.KmlLayer({
     //url: "https://raw.githubusercontent.com/tdeyster/fun_projects/gh-pages/run_where_you_want/running.kml",
     //url: "https://drive.google.com/file/d/15QhrUe99R1gwgBW-t6a81lBS2qk-p9VA/view?usp=sharing",
-    url: "https://raw.githubusercontent.com/tdeyster/fun_projects/gh-pages/run_where_you_want/point2gdrive.kml",
+    //url: "https://raw.githubusercontent.com/tdeyster/fun_projects/gh-pages/run_where_you_want/point2gdrive.kml",
+    url: "https://docs.google.com/uc?authuser=0&id=15QhrUe99R1gwgBW-t6a81lBS2qk-p9VA",
     map: map,
   });
 }
