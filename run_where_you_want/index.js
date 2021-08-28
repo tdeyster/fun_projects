8f9c8876e70b7df82835dbb5f82454b76713a72b
Teddy Eyster <tdeyster@users.noreply.github.com>
2021-08-29 01:22:31 +1200

    Update index.js

--- run_where_you_want/index.js
+++ run_where_you_want/index.js
@@ -5,7 +5,7 @@ function initMap() {
     center: { lat:  42.311, lng: -83.979 },
   });
   const ctaLayer = new google.maps.KmlLayer({
-    url: "http://drive.google.com/uc?id=15QhrUe99R1gwgBW-t6a81lBS2qk-p9VA#t=" + new Date().getTime(),
+    url: "http://drive.google.com/uc?id=1Hl2H4QyOzANoBwHkiS9DKvgWu8sHVPDe#t=" + new Date().getTime(),
     map: map,
   });
 }
