74da8352d2d2cdde9673e770d2fe4ca26edad0ff
Teddy Eyster <tdeyster@users.noreply.github.com>
2022-10-26 04:45:12 -0400

    Update index.js

--- Desert2Greenwood/index.js
+++ Desert2Greenwood/index.js
@@ -1,3 +1,4 @@
+var Googlemap;
 // Initialize google account
     function makeApiCall() {
       var params = {
@@ -76,7 +77,7 @@
 			Number(vals[0]),Number(vals[1]);
 			const runner = new google.maps.Marker({
 	    			position: { lat: lonlat[1], lng: lonlat[0]},
-	    			map,
+	    			map: Googlemap,
 	    			title: "Nice Job!",
 	    			icon: "https://tdeyster.github.io/fun_projects/Desert2Greenwood/runner.png",//"https://icons.iconarchive.com/icons/icons8/windows-8/48/Sports-Running-Man-icon.png",
 	  		});
@@ -85,7 +86,7 @@
 	    		lonlat = [-71.5429,42.50918];
 			const runner = new google.maps.Marker({
 	    			position: { lat: lonlat[1], lng: lonlat[0]},
-	    			map,
+	    			map: Googlemap,
 	    			title: "Nice Job!",
 	    			icon: "https://tdeyster.github.io/fun_projects/Desert2Greenwood/runner.png",//"https://icons.iconarchive.com/icons/icons8/windows-8/48/Sports-Running-Man-icon.png",
 	  		});
@@ -95,13 +96,13 @@
 //////////////////////////////////////////////////////////////////////////////////////
 // [START maps_layer_kml]
 function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
+  Googlemap = new google.maps.Map(document.getElementById("map"), {
     zoom: 11,
     center: { lat:  42.509, lng: -71.543 }
   });
   const ctaLayer = new google.maps.KmlLayer({
     url: "http://drive.google.com/uc?id=1fUTjKFWu1eLpdao2kXo7dc9NwdDP2HSg#t=" + new Date().getTime(),
-    map: map,
+    map: Googlemap,
   });
   //const ctaLayer1 = new google.maps.KmlLayer({
   //  url: "http://drive.google.com/uc?id=15MGMLN9e5SwEtP8XLXFG-Sz41pd8bimZ#t=" + new Date().getTime(),
